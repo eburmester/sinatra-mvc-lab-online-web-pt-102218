886d8888046ea2c46c0e9f0f7172183aa88f40b3
Elyse Burmester <burmesterelyse@gmail.com>
2019-03-13 20:43:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_word = params["user_phrase"]
+    word = PigLatinizer.new
+    @user_phrase = word.to_pig_latin(user_word)
+
+    erb :result
+  end
+  
 end
